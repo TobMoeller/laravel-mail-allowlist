ae654a24e84370e1f437b95f54de923cce663656
TobMoeller <tobiasmoellerw@t-online.de>
2024-09-22 16:44:32 +0200

    wip: remove unnecessary files

--- src/LaravelMailAllowlistServiceProvider.php
+++ src/LaravelMailAllowlistServiceProvider.php
@@ -4,22 +4,13 @@ namespace TobMoeller\LaravelMailAllowlist;
 
 use Spatie\LaravelPackageTools\Package;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
-use TobMoeller\LaravelMailAllowlist\Commands\LaravelMailAllowlistCommand;
 
 class LaravelMailAllowlistServiceProvider extends PackageServiceProvider
 {
     public function configurePackage(Package $package): void
     {
-        /*
-         * This class is a Package Service Provider
-         *
-         * More info: https://github.com/spatie/laravel-package-tools
-         */
         $package
             ->name('laravel-mail-allowlist')
-            ->hasConfigFile()
-            ->hasViews()
-            ->hasMigration('create_laravel_mail_allowlist_table')
-            ->hasCommand(LaravelMailAllowlistCommand::class);
+            ->hasConfigFile();
     }
 }
